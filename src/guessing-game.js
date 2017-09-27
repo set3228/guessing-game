afef74bb57f032bf8cce66cebf3c684d2bc4e4a9
set3228 <2012ynwa@gmail.com>
2017-09-27 21:09:50 +0300

    guessing game solution was added

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return this.guessAnswer = this.min + Math.round((this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.guessAnswer;
     }
 
     greater() {
-
+        this.min = this.guessAnswer;
     }
 }
 
